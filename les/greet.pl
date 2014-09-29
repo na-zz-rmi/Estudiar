b490fe7eecb559fa8547590a619f8f30f1b68941
narmi <narmi@mail.ua>
2014-09-29 07:58:48 +0300

    Create greet.pl

---
+++ les/greet.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+
+use strict;
+use 5.010;
+
+sub greet
+{      state @greeting;
+       foreach my $text( @_ )
+       {
+
+
+
+
+
+            if (@greeting <= 1 )
+            {
+               say  "Hi $text! You are the first one here!";
+               push @greeting, $text;
+
+            }
+            else
+            {
+                say "Hi $text! $greeting[$#greeting] is also here!";
+                push @greeting, $text;
+
+            }
+    }
+}
+
+greet("fedor");
+greet("afanasiy");
